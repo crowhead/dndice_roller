628f9389ba903cc5d102eeb46efdb86e5d79262e
crowhead <maxlewis0@gmail.com>
2014-12-16 15:19:39 -0500

    DnDice Roller: Javascript, HTML, CSS styling elements added
    added CSS file for styling with font, background image. Edited javascript to prepare for modifier checkbox, number of dice input

--- dndice_roller/app/assets/javascripts/dice_roller.js
+++ dndice_roller/app/assets/javascripts/dice_roller.js
@@ -1,6 +1,6 @@
 var DiceRoller = {
-    roll: function(sides) {
-        return Math.ceil(Math.random() * sides);
+    roll: function(sides, modifier, numberOfDie) {
+        return Math.ceil(Math.random() * sides) * numberOfDie + modifier;
     }
 };
 
@@ -10,9 +10,16 @@ $(document).ready(function(){
   $('.dice').click(function(){
     var die = $(this);
     var row = die.parents("tr");
-    var results_display =$(".roll_results", row)
+    var modifier = $(".modifier", row).val();
+    var minus_modifier = $(".minus_modifier", row).val();
+    if (minus_modifier){
+      modifier = modifier * -1
+    }
+    var numberOfDie = $(".numberOfDie", row).val();
+    var results_display = $(".roll_results", row);
     var sides = $(this).data('sides');
-    var roll = DiceRoller.roll(sides);
+    var roll = DiceRoller.roll(sides, modifier, numberOfDie);
+
     results_display.val(roll);
   });
 
