2d6c7f182f6614686dc619ced16ee3f7edf8dd10
crowhead <maxlewis0@gmail.com>
2014-12-17 16:47:34 -0500

    DnDice Roller: added fail and crit messages with audio for d20, css styling modifications
    Added javascript functions to display fail and crit messages for d20 rolls. Modified CSS so that the dice images are passed by CSS into a div in the <td> element

--- dndice_roller/app/assets/javascripts/dice_roller.js
+++ dndice_roller/app/assets/javascripts/dice_roller.js
@@ -14,7 +14,7 @@ var DiceRoller = {
 // dice roll function with modifier and number-of-die calculations
 $(document).ready(function(){
 
-  $('.dice').click(function(){
+  $('.roll_button').click(function(){
     var die = $(this);
     var row = die.parents("tr");
     var modifier = $(".modifier", row).val();
@@ -22,8 +22,27 @@ $(document).ready(function(){
     var results_display = $(".roll_results", row);
     var sides = $(this).data('sides');
     var roll = DiceRoller.roll(sides, modifier, numberOfDie);
-
     results_display.val(roll);
+  });
+
+// fail and crit messages for d20
+  $('.d20').click(function(){
+    var originalDieResult = $(".roll_results", $(this).parents('tr')).val() - $(".modifier", $(this).parents('tr')).val();
+    if (originalDieResult === 20) {
+      var image_tag_crit = '<img src="https://s3-us-west-2.amazonaws.com/dndiceroller/Crit_message.gif">';
+      var crit_audio = '<audio src="http://unrealtournament.99.free.fr/utfiles/sound%20ut/Ownage.wav" id="crit_audio"></audio>';
+      $('.main').append(image_tag_crit);
+      $('.main').append(crit_audio);
+      document.getElementById('crit_audio').play();
+    } else if (originalDieResult === 1) {
+      var image_tag_fail = '<img src="https://s3-us-west-2.amazonaws.com/dndiceroller/fail_message.gif">';
+      var fail_audio = '<audio src="http://wavcentral.com/sounds/televis/price_is_right/tpirhorns.mp3" id="fail_audio"></audio>';
+      $('.main').append(image_tag_fail);
+      $('.main').append(fail_audio);
+      document.getElementById('fail_audio').play();
+    } else {
+      return;
+    }
   });
 
 });
